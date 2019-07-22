1a5a066710acb286a0a869c6e1506b01e9b4a41e
zazaluMonster <hejiajun1432@gmail.com>
2019-07-22 12:11:05 +0800

    初始化提交

---
+++ src/main/java/iona/MyIonaApplication.java
@@ -0,0 +1,13 @@
+package iona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyIonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyIonaApplication.class, args);
+    }
+}
